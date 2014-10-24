5d1163373dc4e8de6dd95112df763816d827efa2
Joe Ranweiler <jkr@galois.com>
2014-10-24 11:36:11 -0700

    Add Show instances for QC

--- src/star-crypto/src/StarVote/Crypto/Types.hs
+++ src/star-crypto/src/StarVote/Crypto/Types.hs
@@ -4,28 +4,40 @@ import Data.Array (Array)
 
 data TEGParams
   = TEGParams {
-        tegGroup     :: DiffieHellmanGroup
-      , tegTrustees  :: Integer
-      , tegThreshold :: Integer
-      }
+    tegGroup     :: DiffieHellmanGroup
+    , tegTrustees  :: Integer
+    , tegThreshold :: Integer
+    }
+  deriving (Show)
 
 -- TODO: Write smart constructor that checks
 -- that 0 < tegThreshold <= tegTrustees
 makeTEGParams = undefined
 
-data TEGPublicKey = TEGPublicKey TEGParams Integer
+data TEGPublicKey
+  = TEGPublicKey TEGParams Integer
+  deriving (Show)
 
-data TEGPrivateKey = TEGPrivateKey TEGParams Integer
+data TEGPrivateKey
+  = TEGPrivateKey TEGParams Integer
+  deriving (Show)
 
-data TEGCipherText = TEGCipherText Integer Integer
+data TEGCipherText
+  = TEGCipherText Integer Integer
+  deriving (Show)
 
-data Shares = Shares (Array Integer Integer)
+data Shares
+  = Shares (Array Integer Integer)
+  deriving (Show)
 
-data DiffieHellmanGroup = DiffieHellmanGroup {
-    dhgSize      :: Integer
-  , dhgOrder     :: Integer
-  , dhgGenerator :: Integer
-  }
+data DiffieHellmanGroup
+  = DiffieHellmanGroup {
+      dhgSize      :: Integer
+    , dhgOrder     :: Integer
+    , dhgGenerator :: Integer
+    }
+  deriving (Show)
+
 
 -- TODO: Write smart constructor that checks that:
 -- -- `size` is positive
