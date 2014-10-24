b291cae7df2ba735df1f17bce218704bc404fbe6
Joe Ranweiler <jkr@galois.com>
2014-10-24 12:13:31 -0700

    Add passing encrypt/decrypt test for simple ElGamal

--- src/star-crypto/test/Test/ThresholdElGamal.hs
+++ src/star-crypto/test/Test/ThresholdElGamal.hs
@@ -1,10 +1,13 @@
 module Test.ThresholdElGamal (thresholdElGamalTests) where
 
+import qualified Data.ByteString.Char8 as B
 
 import Test.Framework
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
+
+import Crypto.Random
 
 import StarVote.Crypto.Groups
 import StarVote.Crypto.Types
@@ -24,8 +27,21 @@ instance Arbitrary TEGParams where
       , tegThreshold = threshold
       }
 
-decryptAfterEncryptIsIdentity = monadicIO $ do
-  return True
+-- TODO: make PropertyM, IO, and Either cohere so no indent drift
+decryptAfterEncryptIsIdentity :: Positive Integer -> Property
+decryptAfterEncryptIsIdentity (Positive msg) = monadicIO $ do
+  rng     <- run (newGenIO :: IO SystemRandom)
+  params  <- pick (arbitrary :: Gen TEGParams)
+  keyPair <- run $ return $ buildKeyPair rng params
+  case keyPair of
+    Left err               -> return False
+    Right ((pk, sk), rng') -> do
+      cipherText <- run $ return $ encryptAsym rng' pk msg
+      case cipherText of
+        Left err -> return False
+        Right (c, _)  -> do
+          p <- return $ decryptAsym sk c
+          return $ p == msg
 
 
 thresholdElGamalTests =
