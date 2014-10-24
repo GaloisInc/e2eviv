70a449569ddc4ba99bf4ada92e220da547dc5a02
Joe Ranweiler <jkr@galois.com>
2014-10-24 10:00:08 -0700

    Make DiffieHellmanGroup a type, define modp2048 as such

--- src/star-crypto/src/StarVote/Crypto/Types.hs
+++ src/star-crypto/src/StarVote/Crypto/Types.hs
@@ -4,12 +4,14 @@ import Data.Array (Array)
 
 data TEGParams
   = TEGParams {
-      tegOrder     :: Integer,
-      tegBits      :: Integer,
-      tegGenerator :: Integer,
-      tegTrustees  :: Integer,
-      tegThreshold :: Integer
-  }
+        tegGroup     :: DiffieHellmanGroup
+      , tegTrustees  :: Integer
+      , tegThreshold :: Integer
+      }
+
+-- TODO: Write smart constructor that checks
+-- that 0 < tegThreshold <= tegTrustees
+makeTEGParams = undefined
 
 data TEGPublicKey = TEGPublicKey TEGParams Integer
 
@@ -18,3 +20,19 @@ data TEGPrivateKey = TEGPrivateKey TEGParams Integer
 data TEGCipherText = TEGCipherText Integer Integer
 
 data Shares = Shares (Array Integer Integer)
+
+data DiffieHellmanGroup = DiffieHellmanGroup {
+    dhgSize      :: Integer
+  , dhgOrder     :: Integer
+  , dhgGenerator :: Integer
+  }
+
+-- TODO: Write smart constructor that checks that:
+-- -- `size` is positive
+-- -- `order` is probable prime
+-- -- `generator` is an integer in [2, order)
+makeDiffieHellmanGroup :: Integer
+                       -> Integer
+                       -> Integer
+                       -> DiffieHellmanGroup
+makeDiffieHellmanGroup = undefined
